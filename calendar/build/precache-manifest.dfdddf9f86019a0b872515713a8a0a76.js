6ec36f1509fb3438ba2ae1d22b4bc05076cef9c7
Devang <acw.dnsp@gmail.com>
2020-11-13 06:13:36 +0000

    fdsgs

---
+++ calendar/build/precache-manifest.dfdddf9f86019a0b872515713a8a0a76.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21022e203245537e66d9b6927e68332a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c97227efeeef0e63ff9e",
+    "url": "/static/css/2.8bd683ae.chunk.css"
+  },
+  {
+    "revision": "ca4c6365da123077f472",
+    "url": "/static/css/main.6567a4ec.chunk.css"
+  },
+  {
+    "revision": "c97227efeeef0e63ff9e",
+    "url": "/static/js/2.c23410f2.chunk.js"
+  },
+  {
+    "revision": "29bcecbc1afd806a39f63d8d172f60ae",
+    "url": "/static/js/2.c23410f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca4c6365da123077f472",
+    "url": "/static/js/main.a95d1ab5.chunk.js"
+  },
+  {
+    "revision": "9fd970e1f25c0cd7eb0a",
+    "url": "/static/js/runtime-main.470b33ba.js"
+  },
+  {
+    "revision": "a587dffd974980a3ce85573c2039c2f0",
+    "url": "/static/media/arrow_down.a587dffd.svg"
+  },
+  {
+    "revision": "1ff60f7c92b9712e4eefe85e1c7f7f51",
+    "url": "/static/media/dashboard.1ff60f7c.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "97954a53c7f59975b13f1cd1002dccc4",
+    "url": "/static/media/list.97954a53.svg"
+  }
+]);
